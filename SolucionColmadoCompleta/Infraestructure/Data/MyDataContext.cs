d356116f745c00859a65350b8a3551ccf0fdafe3
josemiliriano <josemiliriano83@gmail.com>
2026-07-13 19:54:07 -0400

    Creacion Repositorio, ConexionDB Inicio DB

---
+++ SolucionColmadoCompleta/Infraestructure/Data/MyDataContext.cs
@@ -0,0 +1,21 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infraestructure.Data
+{
+    public class MyDataContext : DbContext
+    {
+        public MyDataContext(DbContextOptions<MyDataContext> options) : base(options)
+        {
+
+        }
+        public DbSet<CDCategory> Categories { get; set; }
+        public DbSet<CDProduct> Products { get; set; }
+        public DbSet<CDProvider> Providers { get; set; }
+        public DbSet<CDMovementType> MovementTypes { get; set; }
+        public DbSet<CDInventoryMovement> Movements { get; set; }
+    }
+}
